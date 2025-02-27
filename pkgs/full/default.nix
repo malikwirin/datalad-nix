3f56cbf37645f8c42bd78e5259fddeb1a2082ff5
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-02-27 17:09:39 +0100

    dataladFull: include all maintainers

--- pkgs/full/default.nix
+++ pkgs/full/default.nix
@@ -1,11 +1,21 @@
 { lib, datalad, extensions }:
 
+let
+  # Extracting all maintainers from extensions
+  extensionsMaintainers = lib.flatten (
+    map (ext: ext.meta.maintainers or []) extensions
+  );
+in
 datalad.overrideAttrs (oldAttrs: {
   pname = "dataladFull";
   
   propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ extensions;
 
   meta = oldAttrs.meta // {
-    maintainers = with lib.maintainers; [ malik ] ++ (oldAttrs.meta.maintainers or []);
+    maintainers = lib.unique (
+      with lib.maintainers; [ malik ] ++
+      (oldAttrs.meta.maintainers or []) ++
+      extensionsMaintainers
+    );
   };
 })
