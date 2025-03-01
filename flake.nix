a8496e5d951dca9157921f89d52d9cd06c9efc08
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-03-01 17:38:45 +0100

    Add overlay

--- flake.nix
+++ flake.nix
@@ -7,12 +7,26 @@
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    {
+      overlays = rec {
+        default = datalad;
+
+        datalad = final: prev: 
+          let 
+            packages = import ./pkgs/default.nix { 
+              pkgs = final; 
+              lib = final.lib;
+            };
+          in {
+            datalad-container = packages.container;
+            dataladFull = packages.full;
+          };
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
-    in
-      {
-        packages = import ./pkgs/default.nix { inherit pkgs lib; };
+    in {
+      packages = import ./pkgs/default.nix { inherit pkgs lib; };
       });
 }
