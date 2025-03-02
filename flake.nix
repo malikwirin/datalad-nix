9451687ffa0793ac79ffbe23a24149f9c81c996b
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-03-02 20:43:11 +0100

    Add formatter

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
     {
       overlays = rec {
         default = datalad;
@@ -33,7 +38,17 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
+
+      treefmt = treefmt-nix.lib.evalModule pkgs (import ./treefmt.nix);
     in {
-      packages = import ./pkgs/default.nix { inherit pkgs lib; };
-      });
+      packages = import ./pkgs/default.nix {
+        inherit pkgs lib;
+      };
+
+      formatter = treefmt.config.build.wrapper;
+
+      checks = {
+        formatting = treefmt.config.build.check self;
+      };
+    });
 }
