26a009955d935c8696ec564c4cab51a8673f30f9
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-03-14 14:56:28 +0100

    [DATALAD RUNCMD] Formatting
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "nix fmt",
     "dsid": "90678fd2-5800-4615-b1ae-a7d1bf499862",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [
      "flake.lock",
      "flake.nix",
      "treefmt.nix"
     ],
     "outputs": [
      "*.nix",
      "*/*.nix",
      "*/*/*.nix"
     ],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- checks/test-modules.nix
+++ checks/test-modules.nix
@@ -4,29 +4,29 @@ let
   linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
   darwinSystems = [ "x86_64-darwin" "aarch64-darwin" ];
   allSystems = linuxSystems ++ darwinSystems;
-  
+
   nixosConfigs = import ../examples/nixosConfigurations.nix {
     inherit nixpkgs stateVersion modules;
   };
-  
+
   homeConfigs = import ../examples/homeConfigurations.nix {
     inherit nixpkgs home-manager modules;
   };
-  
-  nixosChecks = lib.genAttrs 
+
+  nixosChecks = lib.genAttrs
     (builtins.filter (system: nixosConfigs ? ${system}) linuxSystems)
     (system: nixosConfigs.${system}.config.system.build.toplevel);
-  
-  homeChecks = lib.genAttrs 
+
+  homeChecks = lib.genAttrs
     (builtins.filter (system: homeConfigs ? ${system}) allSystems)
     (system: homeConfigs.${system}.activationPackage);
-  
-  renamedNixosChecks = lib.mapAttrs' 
-    (name: value: { name = "nixos-${name}"; value = value; }) 
+
+  renamedNixosChecks = lib.mapAttrs'
+    (name: value: { name = "nixos-${name}"; value = value; })
     nixosChecks;
-  
-  renamedHomeChecks = lib.mapAttrs' 
-    (name: value: { name = "home-${name}"; value = value; }) 
+
+  renamedHomeChecks = lib.mapAttrs'
+    (name: value: { name = "home-${name}"; value = value; })
     homeChecks;
 in
 renamedNixosChecks // renamedHomeChecks
