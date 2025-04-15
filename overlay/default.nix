8d4768c9c1ece24c8b82ba25f5f11a9b27da524f
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-04-15 17:43:54 +0200

    overlay: add aneksajo

--- overlay/default.nix
+++ overlay/default.nix
@@ -11,7 +11,7 @@ let
   };
 in
 {
-  dataladGit = packages.dataladGit;
+  inherit (packages) dataladGit forgejo-aneksajo;
 
   datalad-container = packages.container.default;
   datalad-containerGit = packages.container.gitVersion;
