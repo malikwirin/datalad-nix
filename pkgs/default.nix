4bf8d9122345fde668879de46d25fda7ec1f3182
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-05-27 14:03:44 +0200

    [DATALAD RUNCMD] Formatting
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "nix fmt",
     "dsid": "90678fd2-5800-4615-b1ae-a7d1bf499862",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,29 +6,30 @@ in
 rec {
   default = pkgs.datalad;
 
-  dataladGit = default.overrideAttrs (oldAttrs: 
-  let
-    version = "git";
-  in {
-    inherit version;
+  dataladGit = default.overrideAttrs (oldAttrs:
+    let
+      version = "git";
+    in
+    {
+      inherit version;
 
-    src = sources.datalad;
+      src = sources.datalad;
 
-    propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ pkgs.git-annex ];
+      propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ pkgs.git-annex ];
 
-    postPatch = ''
-      sed -i '/def get_versions/,/^$/c\def get_versions():\n    return {"version": "${version}", "full-revisionid": None, "dirty": None, "error": None, "date": None}\n' datalad/_version.py
-   '';
+      postPatch = ''
+        sed -i '/def get_versions/,/^$/c\def get_versions():\n    return {"version": "${version}", "full-revisionid": None, "dirty": None, "error": None, "date": None}\n' datalad/_version.py
+      '';
 
-    meta = oldAttrs.meta // {
-      homepage = "https://github.com/datalad/datalad";
-      maintainers = lib.unique (
-        with lib.maintainers; [ malik ] ++
-          (oldAttrs.meta.maintainers or [ ])
-      );
-      changelog = "";
-    };
-  });
+      meta = oldAttrs.meta // {
+        homepage = "https://github.com/datalad/datalad";
+        maintainers = lib.unique (
+          with lib.maintainers; [ malik ] ++
+            (oldAttrs.meta.maintainers or [ ])
+        );
+        changelog = "";
+      };
+    });
 
   container = import ./container {
     inherit fetchFromGitHub lib python3 git dataladGit;
